0bed253bdb8657fa863141d76219748c7619631c
Selim Dincer <wowselim@live.de>
2021-04-24 12:29:20 +0200

    use fixed aspect ratio in game

--- core/src/main/java/de/ld48/kingfisher/KingfisherGame.java
+++ core/src/main/java/de/ld48/kingfisher/KingfisherGame.java
@@ -5,8 +5,14 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import de.ld48.kingfisher.component.PositionComponent;
 import de.ld48.kingfisher.component.VelocityComponent;
 import de.ld48.kingfisher.system.MovementSystem;
@@ -20,6 +26,10 @@ public class KingfisherGame extends ApplicationAdapter {
     private Entity playerEntity;
     private MovementSystem movementSystem;
 
+    private Camera camera;
+    private Viewport viewport;
+    private Stage stage;
+
     @Override
     public void create() {
         super.create();
@@ -32,11 +42,21 @@ public class KingfisherGame extends ApplicationAdapter {
         engine.addEntity(playerEntity);
         movementSystem = new MovementSystem();
         engine.addSystem(movementSystem);
+
+        camera = new OrthographicCamera(640, 480);
+        viewport = new FitViewport(640, 480, camera);
+        stage = new Stage(viewport);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
     }
 
     @Override
     public void render() {
         super.render();
+        Gdx.gl.glClearColor(1, 1, 1, 0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         engine.update(Gdx.graphics.getDeltaTime());
@@ -49,8 +69,13 @@ public class KingfisherGame extends ApplicationAdapter {
             playerVelocityComponent.x = 100;
         }
 
+        viewport.apply();
+        shapeRenderer.setProjectionMatrix(viewport.getCamera().combined);
+        shapeRenderer.setColor(Color.RED);
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.rect(playerPositionComponent.x, playerPositionComponent.y, 100, 100);
         shapeRenderer.end();
+        stage.getViewport().apply();
+        stage.draw();
     }
 }
