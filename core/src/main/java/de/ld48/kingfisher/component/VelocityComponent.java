a0d7e038d7681987d28bb045743572c6079061fc
Selim Dincer <wowselim@live.de>
2021-04-24 12:09:49 +0200

    add ashley + movement system

---
+++ core/src/main/java/de/ld48/kingfisher/component/VelocityComponent.java
@@ -0,0 +1,8 @@
+package de.ld48.kingfisher.component;
+
+import com.badlogic.ashley.core.Component;
+
+public class VelocityComponent implements Component {
+    public float x;
+    public float y;
+}
